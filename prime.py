6dad420e181a81eadf5fa814d01e27b6990312d0
Hrushikesh <c-s.hrushikesh@capgemini.com>
2023-10-23 21:52:43 +0530

    final changes to prime

--- prime.py
+++ prime.py
@@ -1,8 +1,13 @@
 list=[1,2,3,4,5,6,7]
-for i in range(0,len(list)):
-    for i in range(2,len(list)-1):
-        if(n%i==0):
-            break
+for i in list:
+    if (i==0 or i==1):
+        continue
     else:
-        print("Prime numbers are:"list(i))
+        for j in range(2,i-1):
+            if(i%j==0):
+                break
+        else:
+            print("prime numbers are :",i)
+        
+    
         
